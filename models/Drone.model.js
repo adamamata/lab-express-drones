150ae76e47b9782db6a4647dcb2ce54e2be43ca3
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-08-12 11:21:43 +0200

    iteration 1 complete

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,16 @@
 // Iteration #1
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose; 
+
+const droneSchema = new Schema(
+    {
+        name: {type: String},
+        propellers: {type: Number},
+        maxSpeed: {type: Number}
+    },
+    {
+        timestamps: true      
+    }
+);
+
+module.exports = model("Drone", droneSchema);
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,23 @@
 // Iteration #1
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone.model');
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+
+const MONGO_URL = "mongodb://localhost/lab-express-drones";
+
+mongoose
+    .connect(MONGO_URL)
+    .then(() => console.log(`Connected to Mongo!`))
+    .catch((err) => console.log(err));
+
+Drone.create(drones)
+    .then((data) => {
+        console.log(`Created ${data.length} drones`);
+        mongoose.connection.close;
+    })
+    .catch((err) => console.log(err));
